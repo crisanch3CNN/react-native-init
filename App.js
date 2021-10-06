3c8d9d5d5f316e0ac9af8b21189f220e517adb51
crisanch <cristian.sanchez@triciclos.cl>
2021-10-06 20:31:50 -0300

    feat(hola mundo): personalizar un button

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, View, StyleSheet, Image, Button, Alert } from 'react-native';
+import { Text, View, StyleSheet, Image, Button, Alert, TouchableOpacity } from 'react-native';
 import image from './assets/diamante-rojo.jpg'
 
 const App = () => {
@@ -10,16 +10,23 @@ const App = () => {
         source={{uri: 'https://picsum.photos/200/200'}}
         style={styles.image}
       />
+        <Button
+          color='blue'
+          title="Press"
+          onPress={()=>Alert.alert('hola azul!!')}
+        />
       <Image
         source={image}
         style={styles.image}
       />
 
-      <Button
-        color='blue'
-        title="Press"
-        onPress={()=>Alert.alert('hola!!')}
-      />
+
+      <TouchableOpacity
+        onPress={() => Alert.alert('hola verde!!')}
+        style={ styles.button}
+      >
+        <Text style={ styles.buttonText}>Press Me</Text>
+      </TouchableOpacity>
 
     </View >
   );
@@ -33,7 +40,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#292929',
   },
   title: { fontSize: 30, color: '#ffff' },
-  image: { height: 200, width: 200, borderRadius:100 }
+  image: { height: 200, width: 200, borderRadius:100 },
+  button: { backgroundColor: 'green', padding:7, marginTop:10 },
+  buttonText: { color: '#ffff', fontSize:20 }
 })
 
 export default App;
