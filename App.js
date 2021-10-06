48fbff12a69567c96dbec697cb1f5430f079a67a
crisanch <cristian.sanchez@triciclos.cl>
2021-10-06 20:09:04 -0300

    feat(hola mundo): se agrega una imagen al hola mundo

--- App.js
+++ App.js
@@ -1,22 +1,27 @@
 import React from "react";
-import { Text, View, StyleSheet} from 'react-native';
+import { Text, View, StyleSheet, Image} from 'react-native';
 
 const App = () => {
   return(
     <View style={styles.container}>
-      <Text style={styles.title }>Hola mundo Crisanch!</Text>
+      <Text style={styles.title}>Hola mundo Crisanch!</Text>
+      <Image
+        source={{uri: 'https://picsum.photos/200/200'}}
+        style={styles.image}
+      />
     </View >
   );
 };
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create({ 
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#292929',
   },
-  title: { fontSize: 30, color:'#ffff'}
+  title: { fontSize: 30, color: '#ffff' },
+  image: { height: 200, width: 200 }
 })
 
 export default App;
