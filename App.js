45f4b8146ef0be2f33d0da4b792562dec2845945
crisanch <cristian.sanchez@triciclos.cl>
2021-10-06 19:55:42 -0300

    feat(hola mundo): se agregan estilos al  hola mundo basico

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
 import React from "react";
-import { Text, View} from 'react-native';
+import { Text, View, StyleSheet} from 'react-native';
 
 const App = () => {
   return(
-    <View style={{flex:1, justifyContent:'center', alignItems:'center'} }>
-      <Text style={{ fontSize: 30 }}>Hola mundo!</Text>
+    <View style={styles.container}>
+      <Text style={styles.title }>Hola mundo Crisanch!</Text>
     </View >
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#292929',
+  },
+  title: { fontSize: 30, color:'#ffff'}
+})
 
 export default App;
